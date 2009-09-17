0e4da15de9efabed194e327c40d1c6a364ec6d28
Trygve Laugstøl <trygvis@inamo.no>
2009-09-17 20:52:50 +0200

    ui-snapshot.c: Supposedly the empty initializer is 1) not allowed in C99 and 2) not needed as the array already is a flexible array.

--- ui-snapshot.c
+++ ui-snapshot.c
@@ -39,8 +39,7 @@ const struct cgit_snapshot_format cgit_snapshot_formats[] = {
 	{ ".zip", "application/x-zip", write_zip_archive, 0x1 },
 	{ ".tar.gz", "application/x-gzip", write_tar_gzip_archive, 0x2 },
 	{ ".tar.bz2", "application/x-bzip2", write_tar_bzip2_archive, 0x4 },
-	{ ".tar", "application/x-tar", write_tar_archive, 0x8 },
-	{}
+	{ ".tar", "application/x-tar", write_tar_archive, 0x8 }
 };
 
 static const struct cgit_snapshot_format *get_format(const char *filename)
